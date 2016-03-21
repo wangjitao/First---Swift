7222a8a7db5f7e9fdf00b814eb783b32c8fbde3f
王纪涛 <wangjitao66@163.com>
2016-03-21 15:52:25 +0800

    能够查询~~

--- MYFirstSwiftDemo/Controllers/AddressListViewController.swift
+++ MYFirstSwiftDemo/Controllers/AddressListViewController.swift
@@ -13,7 +13,7 @@ class AddressListViewController: BaseViewController,UITableViewDataSource,UITabl
     var tableView:UITableView?
     var dataSource:NSMutableArray?
     var userDataSource:NSMutableArray?
-    var searchList = [AddressListModel]()
+    var searchList:NSMutableArray?
     var resultSearchUserName:NSMutableArray?
     var searchController:UISearchController?
     
@@ -21,9 +21,12 @@ class AddressListViewController: BaseViewController,UITableViewDataSource,UITabl
         super.viewDidLoad()
         
         rightItemButton("book_addfriend")
+    
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         
         prepareDataAndlayoutSubViewsForAddressListView()
-        
     }
     
     func prepareDataAndlayoutSubViewsForAddressListView() {
@@ -35,7 +38,7 @@ class AddressListViewController: BaseViewController,UITableViewDataSource,UITabl
     func prepareDataForAddressListView() {
         dataSource = NSMutableArray()
         userDataSource = NSMutableArray()
-        //searchList = NSMutableArray()
+        searchList = NSMutableArray()
         let newFriendModel = AddressListModel()
         newFriendModel.userImg = "book_newfriend"
         newFriendModel.userName = "新的朋友"
@@ -51,18 +54,17 @@ class AddressListViewController: BaseViewController,UITableViewDataSource,UITabl
         
         for var num:Int = 0 ; num < 10 ; num++ {
             let userModel = AddressListModel()
-            userModel.userImg = ""
+            userModel.userImg = "image\(num).jpg"
             userModel.userName = "朋友\(num)"
             userDataSource?.addObject(userModel)
         }
         
         dataSource = [[newFriendModel,peopleChatModel,markModel,publicModel],userDataSource!]
-        
     }
     
     func layoutSubViewsForAddressListView() {
         tableView = UITableView()
-        tableView?.frame = CGRectMake(0, 64, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height-64)
+        tableView?.frame = CGRectMake(0, 64, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height-64-49)
         tableView?.delegate = self
         tableView?.dataSource = self
         tableView?.registerNib(UINib(nibName: "AddressListCell", bundle: nil), forCellReuseIdentifier: "AddressListCellId")
@@ -70,10 +72,11 @@ class AddressListViewController: BaseViewController,UITableViewDataSource,UITabl
   
         searchController = UISearchController(searchResultsController: nil)
         searchController?.searchResultsUpdater = self
-        searchController?.dimsBackgroundDuringPresentation = false
-        searchController?.hidesNavigationBarDuringPresentation = false
+        searchController?.dimsBackgroundDuringPresentation = true
+        searchController?.hidesNavigationBarDuringPresentation = true
         searchController?.searchBar.sizeToFit()
         searchController?.searchBar.frame = CGRectMake(searchController!.searchBar.frame.origin.x, searchController!.searchBar.frame.origin.y, searchController!.searchBar.frame.size.width, 44.0)
+        
         searchController?.searchBar.placeholder = "搜索"
         
         searchController?.searchBar.delegate = self
@@ -93,10 +96,10 @@ class AddressListViewController: BaseViewController,UITableViewDataSource,UITabl
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if searchController!.active {
-            
-            return searchList.count
+            return searchList!.count
+        } else {
+            return dataSource![section].count
         }
-        return dataSource![section].count
     }
    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -106,17 +109,20 @@ class AddressListViewController: BaseViewController,UITableViewDataSource,UITabl
         
         //搜索时判断,搜索数组中元素的个数
         if searchController!.active {
-            if searchList.count > 0 {
-                model = searchList[indexPath.row]
+            if searchList!.count > 0 {
+                model = searchList![indexPath.row] as! AddressListModel
                 
             } else {//刚开始输入时,没有符合的直接返回cell
                 return addressCell
             }
         } else {
-            model = dataSource![indexPath.section][indexPath.row] as! AddressListModel
-            
+            if searchList!.count > 0 {
+                model = searchList![indexPath.row] as! AddressListModel
+            } else {
+                model = dataSource![indexPath.section][indexPath.row] as! AddressListModel
+            }
         }
-        
+            
         addressCell.userImageView.image = UIImage(named: model.userImg as!String)
         addressCell.userName.text = model.userName as? String
         
@@ -141,43 +147,40 @@ class AddressListViewController: BaseViewController,UITableViewDataSource,UITabl
         
         let searchPredicate = NSPredicate(format: "SELF CONTAINS [C] %@", searchString)
         
-//        var model = AddressListModel()
-//        
-//        for model in userDataSource! {
-//            
-//        
-//        }
-        
-        if  searchString.length > 0 {
-            //假的 要改
-        
-            let array = (userDataSource! as NSArray).filteredArrayUsingPredicate(searchPredicate)
-            searchList = array as! [AddressListModel]
+        if  searchString.length > 0 {//当有查询条件时
+            //因为searchList里面存的是model,查找时要用名字的数组进行查找,所以将searchList里的model.userName放到全局数组中
+            if resultSearchUserName == nil {
+                resultSearchUserName = NSMutableArray()
+                for model in userDataSource! {
+                    resultSearchUserName!.addObject((model as! AddressListModel).userName!)
+                }
+            }
+            //查询出的用户名字数组
+            let array = (resultSearchUserName! as NSArray).filteredArrayUsingPredicate(searchPredicate)
+            //将查询出的名字数组中的元素与用户model数组的model.userName匹配,并将model加入到searchList中
+            for name in array {
+                for model in userDataSource! {
+                    if name.isEqual((model as! AddressListModel).userName!) {
+                    searchList?.addObject(model)
+                    }
+                }
+            }
+            tableView?.reloadData()
+        } else {//没有查询条件是(只是触发了搜索框)
+            
         }
-        tableView?.reloadData()
     }
     
-//    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
-//        shouldShowSearchResults = true
-//        tableView?.reloadData()
-//    }
-//    
-//    
-//    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
-//        shouldShowSearchResults = false
-//        tableView?.reloadData()
-//    }
-//    
-//    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
-//        print("开始搜索")
-//        return true
-//    }
-//    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
-//        print("开始搜索")
-//    }
-//    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
-//        print("结束搜索")
-//    }
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+       
+        tableView?.reloadData()
+    }
+
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        print("结束搜索")
+       
+        tableView?.reloadData()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
